eaa6d4d75f33d2c41776f8ab157e415729f54fe3
11th floor <t.serhiytschuk@elevenfloor.com>
2018-05-31 11:48:33 +0300

    mi-touch build done

--- commands/build-clm/build-mi-touch.js
+++ commands/build-clm/build-mi-touch.js
@@ -1,16 +1,51 @@
+const fs = require('fs');
+const fsExtra = require('fs-extra');
+const path = require('path');
+const {getFullId, parseSlId} = require('../../lib/util/sl-id-perser');
+const thumbMaker = require('../../lib/thumb-maker');
+const archiveMaker = require('../../lib/archive-maker');
 const webpackSlideBuild = require('../../lib/webpack-slide-builder');
 
-module.exports = async(api, projectOptions) => {
+module.exports = async (api, projectOptions, args, slidesToBuild, clmName) => {
   const startConfig = api.resolveWebpackConfig();
 
-  console.log('build', 'mt');
-  // process.env.VUE_APP_SL_ID = sl.id;
-  // process.env.VUE_APP_SL_PATH = sl.path;
-  // process.env.VUE_APP_OUT_DIR_NAME = sl.id;
-  // process.env.VUE_APP_OUT_HTML_NAME = `${clm.productId}_${sl.id}`;
-  //
-  //
-  // await webpackSlideBuild(api, projectOptions, startConfig)
+  for (let sl of slidesToBuild) {
+    const outSlName = getFullId(sl.id, sl.lang);
 
+    process.env.VUE_APP_CLM = clmName;
+    process.env.VUE_APP_SL_ID = sl.id;
+    process.env.VUE_APP_SL_PATH = sl.path;
+    process.env.VUE_APP_SL_LANG = sl.lang;
+    process.env.VUE_APP_OUT_DIR_PATH = api.resolve(path.join('dist', clmName, sl.lang, outSlName));
+    process.env.VUE_APP_OUT_HTML_NAME = outSlName;
 
+    /** Webpack Build **/
+    await webpackSlideBuild(api, projectOptions, startConfig);
+
+    /** Create thumbnails **/
+    await thumbMaker({
+      width: 200,
+      height: 150,
+      thumbName: `200x150.jpg`,
+      thumbPath: path.join(process.env.VUE_APP_OUT_DIR_PATH, 'media', 'images', 'thumbnails')
+    });
+
+    /** MI Touch special **/
+    createSpecialMiTouchElements();
+
+    /** Create Archive **/
+    await archiveMaker({});
+  }
 };
+
+function createSpecialMiTouchElements() {
+  const slide = {
+    path: process.env.VUE_APP_OUT_DIR_PATH,
+    name: process.env.VUE_APP_OUT_HTML_NAME
+  };
+
+  fsExtra.outputFileSync(path.join(slide.path, 'export', 'export.pdf'));
+
+  const paramsContent = `<Sequence Id="${slide.name}" xmlns="urn:param-schema"></Sequence>`;
+  fsExtra.outputFileSync(path.join(slide.path, 'parameters', 'parameters.xml'), paramsContent);
+}
