7901941a2a560508fd0c5ba4a5b4f2779f0e1cd4
11th floor <t.serhiytschuk@elevenfloor.com>
2018-06-05 16:18:09 +0300

    add navigateTo functional

--- generator/template/src/app/clm/mi-touch.js
+++ generator/template/src/app/clm/mi-touch.js
@@ -2,24 +2,22 @@ import {getFullId} from '@/app/utils/sl-id-parser'
 import desktopNavigationBeyondRootDir from '@/app/utils/desktop-navigation-beyond-root-dir'
 
 
-  export default {
-    created() {
+export default {
+  created() {
     // Disable system vertical fucking swipe
-    // document.addEventListener('touchmove', function (e) {
-    //   e.preventDefault();
-    // }, true);
+    document.addEventListener('touchmove', function (e) {
+      e.preventDefault();
+    }, true);
   },
   methods: {
     navigateTo(id) {
       id = getFullId(id);
-      console.log(id);
 
-      // try {
-      //   // !!!!!!!!! TEMP SOLUTION !!!!!!!!!!!!!!!!
-      //   com.veeva.clm.gotoSlide(id + '.zip', '');
-      // } catch(err) {
-      //   desktopNavigationBeyondRootDir(id);
-      // }
+      try {
+        window.parent.navigateToSequence(id);
+      } catch (err) {
+        desktopNavigationBeyondRootDir(id);
+      }
     }
   }
 }
