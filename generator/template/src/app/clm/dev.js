ba405d6742650dbf36b52f0c9980744d932fd98d
11th floor <t.serhiytschuk@elevenfloor.com>
2018-06-05 16:05:36 +0300

    remove console.log

--- generator/template/src/app/clm/dev.js
+++ generator/template/src/app/clm/dev.js
@@ -1,14 +1,9 @@
 import Router from '@/app/router'
-import { getFullId } from '../utils/sl-id-parser';
 
 export default {
-  created() {
-    console.log('LOL')
-  },
   methods: {
     navigateTo(id) {
-      console.log(getFullId(id, this.$store.state.lang));
-      // Router.push(`/${id}`)
+      Router.push(`/${id}`)
     }
   }
 }
