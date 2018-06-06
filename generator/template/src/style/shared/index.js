c91789f4f9d24c60f583f7d49fd5c7bbd94cb4b1
Taras <tarasserhiytschuk@gmail.com>
2018-06-06 21:29:16 +0300

    change method for include resolution in shared styles

--- generator/template/src/style/shared/index.js
+++ generator/template/src/style/shared/index.js
@@ -1,20 +1,20 @@
 /**
- * The file enables return all scss/sass files to import as one sass module
+ * The file return all scss/sass files data and include '$width and $height' from 'clm.config.js'
  * in a one-shot manner. There should not be any reason to edit this file.
  */
 
 const fs = require('fs');
-const {width, height} = require('../../clm.config').device.resolution;
 
-module.exports = fs.readdirSync(__dirname).map(file => {
-  // Include resolution
-  if (file === 'variables.scss') {
-    return fs.readFileSync(`${__dirname}\\${file}`, 'utf8')
-      .replace('29051994em', width + 'px;')
-      .replace('29051994rem', height + 'px;')
-  }
-
+let sharedData = fs.readdirSync(__dirname).map(file => {
   return /\.s(a|c)ss$/g.test(file) // Check if file extension is .sass or .scss
     ? fs.readFileSync(`${__dirname}\\${file}`, 'utf8')
     : '';
 }).join('\n');
+
+// Include resolution
+const {width, height} = require('../../clm.config').device.resolution;
+
+sharedData += `$width: ${width}px;`;
+sharedData += `$height: ${height}px;`;
+
+module.exports = sharedData;
