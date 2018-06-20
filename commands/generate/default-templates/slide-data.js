9cdf30b1fe3f9357e9b1615a8edefaa701bc6d7b
Taras <tarasserhiytschuk@gmail.com>
2018-06-20 12:38:02 +0300

    remove cooments

--- commands/generate/default-templates/slide-data.js
+++ commands/generate/default-templates/slide-data.js
@@ -1,11 +1,9 @@
 export default {
-  // This file was automatically imported in slide-component with the same file name
-  // You have all this data in computed property "data" and content data in computed property "t"
-  content: {
-    title: ''
-  },
+	content: {
+		title: ''
+	},
 
-  popup: {
-    references: {}
-  }
+	popup: {
+		references: {}
+	}
 }
