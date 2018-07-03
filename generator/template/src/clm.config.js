31c696f2c17ab54850a2fa7ee63698206b2fa6b3
Taras <tarasserhiytschuk@gmail.com>
2018-07-03 17:18:39 +0300

    add some info

--- generator/template/src/clm.config.js
+++ generator/template/src/clm.config.js
@@ -1,25 +1,72 @@
 module.exports = {
-  // CLM platform options
   clm: {
-    productId: 'TEST_C2_18' // => [PROJECT-NAME]_[CYCLE]_[YEAR]
+    productId: 'TEST_C2_18', // => <PROJECT-NAME>_<CYCLE>_<YEAR>
+    disableSwipeBetweenFlows: true // Auto preventing swipes between flows
   },
 
-  // valid values: ua, ru, en
-  languages: ['ua', 'ru'],
+  languages: ['ua', 'ru'], // Valid values: ua, ru, en
 
   device: {
     resolution: {
-      width: 2048,
-      height: 1536
+      width: 1280,
+      height: 720
     }
   },
 
-  // Each object must have following keys: 'id', 'path', 'name'
+  // Required keys: 'id', 'path', 'name'.
+  // Optionally keys: 'flowName', 'swipe'
   structure: [
     {
       id: 'slide-main',
       path: 'slides/slide-main',
-      name: {ua: 'Назва', ru: 'Название'}
+      name: {ua: 'Назва', ru: 'Название'},
+    },
+
+    {
+      id: 'slide-1_1',
+      path: 'slides/slide-1_1',
+      name: {ua: 'Назва', ru: 'Название'},
+      flowName: {ua: 'Flow-1 ua', ru: 'Flow-1 ru'},
+      swipe: {
+        next: 'slide-3_2',
+        prev: 'prevent'
+      }
+    },
+    {
+      id: 'slide-1_2',
+      path: 'slides/slide-1_2',
+      name: {ua: 'Назва', ru: 'Название'},
+    },
+
+
+    {
+      id: 'slide-2_1',
+      path: 'slides/slide-2_1',
+      name: {ua: 'Назва', ru: 'Название'},
+      flowName: 'Flow - 2',
+      swipe: {
+        prev: 'slide-1_1'
+      }
+    },
+    {
+      id: 'slide-2_2',
+      path: 'slides/slide-2_2',
+      name: {ua: 'Назва', ru: 'Название'},
+    },
+
+
+    {
+      id: 'slide-3_1',
+      path: 'slides/slide-3_1',
+      name: {ua: 'Назва', ru: 'Название'},
+    },
+    {
+      id: 'slide-3_2',
+      path: 'slides/slide-3_2',
+      name: {ua: 'Назва', ru: 'Название'},
+      swipe: {
+        prev: 'slide-1_1'
+      }
     }
   ]
 };
